d1d88cb712ee47e5f1826c7012e93bf3c9cc741d
RyanCSCourse <122550188+RyanCSCourse@users.noreply.github.com>
2025-06-10 13:35:32 -0400

    Added some menus (10/06/25)

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,21 @@ public class Main {
         //
 
         // initial variables
-        // Arraylist holds the cart items
+        // Arraylist holds the cart items and their properties
+        // The arraylist takes the ItemName, Type, Cost and Desc and compresses it to
+        // one string which then gets redistributed when viewed
         ArrayList<String> cartItems = new ArrayList<>();
-        ArrayList<Integer> cartCosts = new ArrayList<>();
+        cartItems.add("Homemade PC^Item^1500^homemade pc I7-1500k, 4070ti, 1tb");
+
+        // Listings
+        ArrayList<String> listings = new ArrayList<>();
+        // Presiquite
+        listings.add("Homemade PC^Item^1500^homemade pc I7-1500k, 4070ti, 1tb");
+
         Scanner sc = new Scanner(System.in);
 
         // newestEntry is made to keep the newestKey/Entry pressed
-        for (int newestEntry = 0; newestEntry < 98; newestEntry = sc.nextInt()) {
+        for (int newestEntry = 0; newestEntry < 98; newestEntry = Integer.parseInt(sc.nextLine())) {
             //
             // switch case to select different options
             // 0=Defualt
@@ -54,12 +62,65 @@ public class Main {
                 case 1:
                     System.out.println(String.format("\nCart:"));
                     if (cartItems.size() == 0) {
-                        System.out.println(String.format("!: \tYou have no items in cat :("));
+                        System.out.println(String.format("!: You have no items in cart :("));
+                    } else {
+                        // fortmat items in cart
+                        for (String cartitem : cartItems) {
+                            // Cart Decription!!!!!
+                            int posNamount = 0;
+                            String itemCartName = "";
+                            String itemCarttype = "";
+                            String itemCartCost = "";
+                            String itemCartDesc = "";
+                            for (int letterPOS = 0; letterPOS < cartitem.length(); letterPOS++) {
+                                // Make Name
+                                if (cartitem.charAt(letterPOS) == '^') {
+                                    posNamount += 1;
+                                } else {
+                                    // Organize values to variables
+                                    switch (posNamount) {
+                                        case 0:
+                                            itemCartName += cartitem.charAt(letterPOS);
+                                            break;
+                                        case 1:
+                                            itemCarttype += cartitem.charAt(letterPOS);
+                                            break;
+                                        case 2:
+                                            itemCartCost += cartitem.charAt(letterPOS);
+                                            break;
+                                        case 3:
+                                            itemCartDesc += cartitem.charAt(letterPOS);
+                                            break;
+                                    }
+                                }
+
+                            }
+                            System.out.println(itemCartCost);
+                            System.out.println(itemCarttype);
+                            System.out.println(itemCartName);
+                            System.out.println(itemCartDesc);
+
+                        }
                     }
-
-
                     break;
 
+                /// View Add Services
+                case 2:
+                    // this is getting the name
+                    System.out.println("what is the product/services name?\n");
+                    String ServAddName = sc.nextLine();
+                    
+                    System.out.println("what type of service are you offering (product or service)\n");
+                    String ServAddType = sc.nextLine();
+
+                    System.out.println("add a description ");
+                    String ServAddDesc = sc.nextLine();
+
+                    System.out.println("What is the price");
+                    int ServAddPrice = sc.nextInt();
+                    listings.add(String.format("%s^%s^%s^%s", ServAddName, ServAddType, ServAddPrice, ServAddDesc));
+                    cartItems.add(String.format("%s^%s^%s^%s", ServAddName, ServAddType, ServAddPrice, ServAddDesc));
+                    break;
             }
 
         }
